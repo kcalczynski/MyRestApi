67c278a31292aa50b9f0eeb039873a4a74749903
Krzysztof Calczynski <calczynski.krzysztof@gmail.com>
2016-11-19 09:07:24 +0100

    few modification

--- src/main/java/com/MyRestApiGroup/entity/Task.java
+++ src/main/java/com/MyRestApiGroup/entity/Task.java
@@ -1,10 +1,9 @@
 package com.MyRestApiGroup.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-
-import static java.lang.Boolean.FALSE;
 
 @Entity
 @Table(name = "TASKS")
@@ -15,27 +14,30 @@ public class Task {
             parameters = @org.hibernate.annotations.Parameter(name="sequence", value="TASKS_SEQ"))
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "taskSequence")
     @Column(name = "TASK_ID")
+    @JsonProperty
     private Long id;
 
     @Column(name = "TASK_NAME")
-    private String Name;
+    @JsonProperty
+    private String name;
 
     @Column(name = "TASK_DESC")
-    private String Description;
+    @JsonProperty
+    private String description;
 
     @Column(name = "TASK_STAT", columnDefinition="BIT")
-    private boolean Status;
+    @JsonProperty
+    private boolean status;
 
     public Task(Long id, String name, String description, boolean status) {
         this.id = id;
-        Name = name;
-        Description = description;
-        Status = status;
+        this.name = name;
+        this.description = description;
+        this.status = status;
     }
     public Task(String name, String description) {
-        Name = name;
-        Description = description;
-        Status = FALSE;
+        this.name = name;
+        this.description = description;
     }
 
     public Task(){
@@ -50,27 +52,27 @@ public class Task {
     }
 
     public String getName() {
-        return Name;
+        return name;
     }
 
     public void setName(String name) {
-        Name = name;
+        this.name = name;
     }
 
     public String getDescription() {
-        return Description;
+        return description;
     }
 
     public void setDescription(String description) {
-        Description = description;
+        this.description = description;
     }
 
     public boolean isStatus() {
-        return Status;
+        return status;
     }
 
     public void setStatus(boolean status) {
-        Status = status;
+        this.status = status;
     }
 
     @Override
@@ -80,19 +82,19 @@ public class Task {
 
         Task task = (Task) o;
 
-        if (Status != task.Status) return false;
+        if (status != task.status) return false;
         if (id != null ? !id.equals(task.id) : task.id != null) return false;
-        if (Name != null ? !Name.equals(task.Name) : task.Name != null) return false;
-        return Description != null ? Description.equals(task.Description) : task.Description == null;
+        if (name != null ? !name.equals(task.name) : task.name != null) return false;
+        return description != null ? description.equals(task.description) : task.description == null;
 
     }
 
     @Override
     public int hashCode() {
         int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (Name != null ? Name.hashCode() : 0);
-        result = 31 * result + (Description != null ? Description.hashCode() : 0);
-        result = 31 * result + (Status ? 1 : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (status ? 1 : 0);
         return result;
     }
 }
